4153956ca7dfbd1ad98b4e18f9a15fabf69241df
Maciej Zieliński <402650@student.agh.edu.pl>
2022-09-25 00:05:13 +0200

    added three basic parameters

--- frontend/src/app/services/parameters.service.ts
+++ frontend/src/app/services/parameters.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
+import {MatSliderChange} from "@angular/material/slider";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParametersService {
   minerNodes = 10;
+  price: any;
+  frequency: any;
+  award: any;
+
+  constructor() { }
 
   public getMinerNodes(): number {
     return this.minerNodes;
   }
 
-
   public setMinerNodes(value: number): void {
     this.minerNodes = value;
   }
 
-  constructor() { }
+  setPrice(value: any) {
+    this.price = value;
+  }
+
+  setFrequency(value: any) {
+    this.frequency = value;
+  }
+
+  setAward(value: any) {
+    this.award = value;
+  }
+
+
 }
