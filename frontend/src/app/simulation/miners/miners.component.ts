92895d9d20843bfab7cb5dabd5f3b2264e1e0004
Maciej Zieliński <402650@student.agh.edu.pl>
2022-10-16 19:20:29 +0200

    added pagination and sorting to miners table

--- frontend/src/app/simulation/miners/miners.component.ts
+++ frontend/src/app/simulation/miners/miners.component.ts
@@ -1,32 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {SimulationService} from "../../services/simulation.service";
 import {Node} from "../model/node";
 import {catchError, of, tap} from "rxjs";
 import {MinerService} from "../../services/miner.service";
-import {defaultLogger} from "@angular/cdk/schematics/update-tool/logger";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-miners',
   templateUrl: './miners.component.html',
   styleUrls: ['./miners.component.scss']
 })
-export class MinersComponent implements OnInit {
+export class MinersComponent implements AfterViewInit {
 
-  minerList: Node[];
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+
+  minerList = new MatTableDataSource<Node>();
   displayedColumns = ['id', 'money', 'mined', 'length']
 
   constructor(
     private simulationService: SimulationService,
     private minerService: MinerService
   ) {
-    this.minerList = this.simulationService.getMiners();
+    this.minerList.data = this.simulationService.getMiners();
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.minerList.paginator = this.paginator;
+    this.minerList.sort = this.sort;
+    this.getData();
+  }
+
+  getData(): void {
     this.minerService.get()
       .pipe(
         tap( () => {
-          this.minerList = this.simulationService.getMiners();
+          this.minerList.data = this.simulationService.getMiners();
         }),
         catchError(err => {
           console.log(err.error.error);
