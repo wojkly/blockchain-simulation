52b3483911a4a01adb614f4b2a64ca4695beb239
Maciej Zieliński <402650@student.agh.edu.pl>
2022-10-16 18:54:24 +0200

    refactored initial-params and added button

--- frontend/src/app/initial-configuration/initial-configuration.component.ts
+++ frontend/src/app/initial-configuration/initial-configuration.component.ts
@@ -4,7 +4,6 @@ import {SIMULATION_PATH} from "../app-routing-paths";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {ParametersService} from "../services/parameters.service";
-import {SimulationService} from "../services/simulation.service";
 
 @Component({
   selector: 'app-initial-configuration',
@@ -12,10 +11,10 @@ import {SimulationService} from "../services/simulation.service";
   styleUrls: ['./initial-configuration.component.scss']
 })
 export class InitialConfigurationComponent implements OnInit {
-  nodesType1FC: FormControl = new FormControl(1, [Validators.required, Validators.min(1)]);
-  nodesType2FC: FormControl = new FormControl(1, [Validators.required, Validators.min(1)]);
-  nodesType3FC: FormControl = new FormControl(1, [Validators.required, Validators.min(1)]);
-  nodesType4FC: FormControl = new FormControl(1, [Validators.required, Validators.min(1)]);
+  nodesType1FC: FormControl = new FormControl(this.parametersService.getFullNodes(), [Validators.required, Validators.min(1)]);
+  nodesType2FC: FormControl = new FormControl(this.parametersService.getLightNodes(), [Validators.required, Validators.min(1)]);
+  nodesType3FC: FormControl = new FormControl(this.parametersService.getMinerNodes(), [Validators.required, Validators.min(1)]);
+  nodesType4FC: FormControl = new FormControl(this.parametersService.getLightNodes(), [Validators.required, Validators.min(1)]);
 
   form = this.formBuilder.group({
     nodesType1FC: this.nodesType1FC,
@@ -30,21 +29,21 @@ export class InitialConfigurationComponent implements OnInit {
               private router: Router,
               private formBuilder: FormBuilder,
               private snackBar: MatSnackBar,
-              private parametersService: ParametersService,
-              private simulationService: SimulationService) {
+              private parametersService: ParametersService) {
   }
 
   ngOnInit(): void {
+    this.numberOfNodes = this.parametersService.getAllNodes();
+  }
+
+  formValidation(){
+    return this.nodesType1FC.valid && this.nodesType2FC.valid && this.nodesType3FC.valid && this.nodesType4FC.valid
   }
 
   onClick(): void {
-    if (!(this.nodesType1FC.valid && this.nodesType2FC.valid && this.nodesType3FC.valid && this.nodesType4FC.valid)) {
-      this.snackBar.open("Błędnie wypełniony formularz!",'',{ duration: 500 });
-    } else {
-      this.snackBar.open("Przechodzenie do symulacji",'',{ duration: 500 });
-      this.setNodes();
-      this.router.navigate([SIMULATION_PATH], {relativeTo: this.activatedRoute.parent});
-    }
+    this.snackBar.open("Przechodzenie do symulacji",'',{ duration: 500 });
+    this.setNodes();
+    this.router.navigate([SIMULATION_PATH], {relativeTo: this.activatedRoute.parent});
   }
 
   setNodes(): void {
